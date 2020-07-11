3e1327c16b447bacf4cee94c39530544a55a9afc
Milagros932 <milinr29@gmail.com>
2020-07-10 22:13:00 -0500

    setup con angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-raiz',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+// define la funcionalidad del componente
+export class AppComponent {
+  title = 'BQ API';
+}

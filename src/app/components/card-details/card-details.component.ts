0996b295120fa5c355003df5e6545f5a1e8109ce
greciagarciav <garciav.grecia@gmail.com>
2020-07-15 03:43:37 -0500

    fix views

--- src/app/components/card-details/card-details.component.ts
+++ src/app/components/card-details/card-details.component.ts
@@ -6,7 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-details.component.scss']
 })
 export class CardDetailsComponent implements OnInit {
-
+  waitersStaff:any[] = [
+    {
+      photo: 'assets/images/icon-yellow/waiter.png',
+      nombres: 'Mey',
+      email:'paulina@burguerqueen.com',
+      birthday: '24/10/1996',
+      cellphone: '958641235'
+    },
+    {
+      photo: 'assets/images/icon-yellow/waiter.png',
+      nombres: 'Lore',
+      email:'paulina@burguerqueen.com',
+      birthday: '24/10/1996',
+      cellphone: '958641235'
+    },
+    {
+      photo: 'assets/images/icon-yellow/waiter.png',
+      nombres: 'Ross',
+      email:'paulina@burguerqueen.com',
+      birthday: '24/10/1996',
+      cellphone: '958641235'
+    },
+    {
+      photo: 'assets/images/icon-yellow/waiter.png',
+      nombres: 'Maya',
+      email:'paulina@burguerqueen.com',
+      birthday: '24/10/1996',
+      cellphone: '958641235'
+    },
+    {
+      photo: 'assets/images/icon-yellow/waiter.png',
+      nombres: 'Maria',
+      email:'paulina@burguerqueen.com',
+      birthday: '24/10/1996',
+      cellphone: '958641235'
+    },
+    {
+      photo: 'assets/images/icon-yellow/waiter.png',
+      nombres: 'Meche',
+      email:'paulina@burguerqueen.com',
+      birthday: '24/10/1996',
+      cellphone: '958641235'
+    }
+    ]
   constructor() { }
 
   ngOnInit(): void {

879426d0daa0daf8907014a5a9983fd3ae5d7444
greciagarciav <garciav.grecia@gmail.com>
2020-07-15 00:27:21 -0500

    create components of waiters and chefs

--- src/app/components/vertical-menu/vertical-menu.component.ts
+++ src/app/components/vertical-menu/vertical-menu.component.ts
@@ -6,31 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vertical-menu.component.scss']
 })
 export class VerticalMenuComponent implements OnInit {
-menuAdmin:any[] = [{
+menuAdmin:any[] = [
+{
   title: 'Home',
   icon:'assets/images/icon-yellow/home.png',
   href: '/home'
-},{
+},
+{
   title: 'Meseros',
-  icon:'assets/images/icon-white/image (8).png',
-  href: '/second-component'
-},{
+  icon:'assets/images/icon-white/waiter.png',
+  href: '/waiters-list'
+},
+{
   title: 'Cocineros',
-  icon:'assets/images/icon-white/image (8).png',
-  href: 'admin/chefs-list'
-},{
+  icon:'assets/images/icon-white/chef.png',
+  href: '/chefs-list'
+},
+{
   title: 'Menu',
-  icon:'assets/images/icon-white/image (9).png',
+  icon:'assets/images/icon-white/menu.png',
   href: '/second-component'
-},{
+},
+{
   title: 'Inventario',
-  icon:'assets/images/icon-white/image (8).png',
+  icon:'assets/images/icon-white/inventory.png',
   href: '/second-component'
-},{
+},
+{
   title: 'Salir',
-  icon:'assets/images/icon-white/image (10).png',
+  icon:'assets/images/icon-white/logout.png',
   href: '/second-component'
-}]
+}
+]
 
   constructor() { }
 

241a6a0cb7194637eec4209148a4eb77fb8d0c1d
Milagros932 <milinr29@gmail.com>
2020-07-18 22:22:38 -0500

    add dinamic data to cards

--- src/app/components/card-details/card-details.component.ts
+++ src/app/components/card-details/card-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { JsonApiServiceService } from '../../JsonApiService.service'
 
 @Component({
   selector: 'app-card-details',
@@ -6,52 +7,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-details.component.scss']
 })
 export class CardDetailsComponent implements OnInit {
-  waitersStaff:any[] = [
-    {
-      photo: 'assets/images/icon-yellow/waiter.png',
-      nombres: 'Mey',
-      email:'paulina@burguerqueen.com',
-      birthday: '24/10/1996',
-      cellphone: '958641235'
-    },
-    {
-      photo: 'assets/images/icon-yellow/waiter.png',
-      nombres: 'Lore',
-      email:'paulina@burguerqueen.com',
-      birthday: '24/10/1996',
-      cellphone: '958641235'
-    },
-    {
-      photo: 'assets/images/icon-yellow/waiter.png',
-      nombres: 'Ross',
-      email:'paulina@burguerqueen.com',
-      birthday: '24/10/1996',
-      cellphone: '958641235'
-    },
-    {
-      photo: 'assets/images/icon-yellow/waiter.png',
-      nombres: 'Maya',
-      email:'paulina@burguerqueen.com',
-      birthday: '24/10/1996',
-      cellphone: '958641235'
-    },
-    {
-      photo: 'assets/images/icon-yellow/waiter.png',
-      nombres: 'Maria',
-      email:'paulina@burguerqueen.com',
-      birthday: '24/10/1996',
-      cellphone: '958641235'
-    },
-    {
-      photo: 'assets/images/icon-yellow/waiter.png',
-      nombres: 'Meche',
-      email:'paulina@burguerqueen.com',
-      birthday: '24/10/1996',
-      cellphone: '958641235'
-    }
-    ]
-  constructor() { }
+  // waitersStaff:any[] = [
+  //   {
+  //     photo: 'assets/images/icon-yellow/waiter.png',
+  //     nombres: 'Mey',
+  //     email:'paulina@burguerqueen.com',
+  //     birthday: '24/10/1996',
+  //     cellphone: '958641235'
+  //   },
+  //   {
+  //     photo: 'assets/images/icon-yellow/waiter.png',
+  //     nombres: 'Lore',
+  //     email:'paulina@burguerqueen.com',
+  //     birthday: '24/10/1996',
+  //     cellphone: '958641235'
+  //   },
+  //   {
+  //     photo: 'assets/images/icon-yellow/waiter.png',
+  //     nombres: 'Ross',
+  //     email:'paulina@burguerqueen.com',
+  //     birthday: '24/10/1996',
+  //     cellphone: '958641235'
+  //   },
+  //   {
+  //     photo: 'assets/images/icon-yellow/waiter.png',
+  //     nombres: 'Maya',
+  //     email:'paulina@burguerqueen.com',
+  //     birthday: '24/10/1996',
+  //     cellphone: '958641235'
+  //   },
+  //   {
+  //     photo: 'assets/images/icon-yellow/waiter.png',
+  //     nombres: 'Maria',
+  //     email:'paulina@burguerqueen.com',
+  //     birthday: '24/10/1996',
+  //     cellphone: '958641235'
+  //   },
+  //   {
+  //     photo: 'assets/images/icon-yellow/waiter.png',
+  //     nombres: 'Meche',
+  //     email:'paulina@burguerqueen.com',
+  //     birthday: '24/10/1996',
+  //     cellphone: '958641235'
+  //   }
+  //   ]
 
+  //funcion para obtener no admnistrador
+  findEmployer = (employer: any): any => employer.roles.admin === false
+  // varialbe data almacena array de los meseros
+  data: any;
+  
+  constructor(public json: JsonApiServiceService) {
+    this.json.getJson('http://localhost:3000/users#').subscribe((response: any) => {
+
+      this.data = response.filter(this.findEmployer)
+      console.log(this.data)
+    })
+  }
   ngOnInit(): void {
   }
 

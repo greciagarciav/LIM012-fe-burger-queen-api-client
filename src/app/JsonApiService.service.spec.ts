07a99502d63a0264193836e7460447b333738dc4
Milagros932 <milinr29@gmail.com>
2020-07-20 10:37:28 -0500

    add users function

--- src/app/JsonApiService.service.spec.ts
+++ src/app/JsonApiService.service.spec.ts
@@ -1,16 +1,16 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
-import { JsonApiServiceService } from './JsonApiService.service';
+import { JsonApiService } from './JsonApiService.service';
 
 describe('Service: JsonApiService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [JsonApiServiceService]
+      providers: [JsonApiService]
     });
   });
 
-  it('should ...', inject([JsonApiServiceService], (service: JsonApiServiceService) => {
+  it('should ...', inject([JsonApiService], (service: JsonApiService) => {
     expect(service).toBeTruthy();
   }));
 });
--- src/app/components/card-details/card-details.component.ts
+++ src/app/components/card-details/card-details.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { JsonApiServiceService } from '../../JsonApiService.service'
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { JsonApiService } from '../../JsonApiService.service'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-card-details',
@@ -7,64 +8,57 @@ import { JsonApiServiceService } from '../../JsonApiService.service'
   styleUrls: ['./card-details.component.scss']
 })
 export class CardDetailsComponent implements OnInit {
-  // waitersStaff:any[] = [
-  //   {
-  //     photo: 'assets/images/icon-yellow/waiter.png',
-  //     nombres: 'Mey',
-  //     email:'paulina@burguerqueen.com',
-  //     birthday: '24/10/1996',
-  //     cellphone: '958641235'
-  //   },
-  //   {
-  //     photo: 'assets/images/icon-yellow/waiter.png',
-  //     nombres: 'Lore',
-  //     email:'paulina@burguerqueen.com',
-  //     birthday: '24/10/1996',
-  //     cellphone: '958641235'
-  //   },
-  //   {
-  //     photo: 'assets/images/icon-yellow/waiter.png',
-  //     nombres: 'Ross',
-  //     email:'paulina@burguerqueen.com',
-  //     birthday: '24/10/1996',
-  //     cellphone: '958641235'
-  //   },
-  //   {
-  //     photo: 'assets/images/icon-yellow/waiter.png',
-  //     nombres: 'Maya',
-  //     email:'paulina@burguerqueen.com',
-  //     birthday: '24/10/1996',
-  //     cellphone: '958641235'
-  //   },
-  //   {
-  //     photo: 'assets/images/icon-yellow/waiter.png',
-  //     nombres: 'Maria',
-  //     email:'paulina@burguerqueen.com',
-  //     birthday: '24/10/1996',
-  //     cellphone: '958641235'
-  //   },
-  //   {
-  //     photo: 'assets/images/icon-yellow/waiter.png',
-  //     nombres: 'Meche',
-  //     email:'paulina@burguerqueen.com',
-  //     birthday: '24/10/1996',
-  //     cellphone: '958641235'
-  //   }
-  //   ]
 
-  //funcion para obtener no admnistrador
-  findEmployer = (employer: any): any => employer.roles.admin === false
-  // varialbe data almacena array de los meseros
-  data: any;
+  //variable que se va a ir al componente padre
+  // @Output() cambio = new EventEmitter<number>();
+  // cambiar() {
+  //   this.cambio.emit(5)
+  // }
   
-  constructor(public json: JsonApiServiceService) {
-    this.json.getJson('http://localhost:3000/users#').subscribe((response: any) => {
+  // cambiar(){
+  //   this.cambio.emit({"dato"})
+  // }
 
+
+  
+data: any; // varialbe data almacena array de los meseros
+  url: string = 'http://localhost:3000/users#'
+  constructor(public json: JsonApiService) {
+  }
+  newPerson: any = {
+    "id": "85",
+    "email": "carlos@gmail.com",
+    "roles": {
+      "admin": false
+    },
+    "photo": "string.png",
+    "name": " Garcia",
+    "dateBirth": "04/10/1996",
+    "cellphone": "944444444"
+  }
+  id= "85821"
+  findEmployer = (employer: any): any => employer.roles.admin === false//funcion para obtener no admnistrador
+  
+  addEmployed() {
+    this.json.postEmployed(this.url, this.newPerson).subscribe((response: any) => {
+      this.data.push(response)
+    });
+  }
+
+  lessEmployed():void{
+      this.json.deleteEmployed(this.url, this.id).subscribe((response: any) => {
+      console.log(response);
+      
+      }); 
+      this.data = this.data.filter(e=>e.id !==this.id)
+  }
+
+  ngOnInit(): void {
+    this.json.getEmployed(this.url).subscribe((response: any) => {
       this.data = response.filter(this.findEmployer)
       console.log(this.data)
     })
   }
-  ngOnInit(): void {
-  }
+
 
 }

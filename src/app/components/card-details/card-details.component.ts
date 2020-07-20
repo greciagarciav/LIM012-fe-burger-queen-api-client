5ec0644789c4d0ea5e47e97d158f6ae7c02e83fd
greciagarciav <garciav.grecia@gmail.com>
2020-07-20 08:39:54 -0500

    pass data trhough father to child

--- src/app/components/card-details/card-details.component.ts
+++ src/app/components/card-details/card-details.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { JsonApiServiceService } from '../../JsonApiService.service'
+// import { JsonApiServiceService } from '../../JsonApiService.service'
 
 @Component({
   selector: 'app-card-details',
@@ -8,23 +8,36 @@ import { JsonApiServiceService } from '../../JsonApiService.service'
   styleUrls: ['./card-details.component.scss']
 })
 export class CardDetailsComponent implements OnInit {
-  showModal = false;
+  // @Input() id: string;
+  // @Input() photo: string;
+  // @Input() name: string;
+  // @Input() email: string;
+  // @Input() dateBirth: string;
+  // @Input() cellphone: string;
+  @Input() data: any;
 
+
+  showModal = false;
   toggleModal = () => {
     this.showModal = !this.showModal;
   }
-  //funcion para obtener no admnistrador
-  findEmployer = (employer: any): any => employer.roles.admin === false
-  // varialbe data almacena array de los meseros
-  data: any;
+  // //funcion para obtener no admnistrador
+  // findEmployer = (employer: any): any => employer.roles.admin === false
+  // // varialbe data almacena array de los meseros
+  // data: any;
 
-  constructor(public json: JsonApiServiceService, private route: ActivatedRoute) {
-    this.json.getJson('http://localhost:3000/users#').subscribe((response: any) => {
+  // constructor(public json: JsonApiServiceService, private route: ActivatedRoute) {
+  //   this.json.getJson('http://localhost:3000/users#').subscribe((response: any) => {
 
-      this.data = response.filter(this.findEmployer)
-      console.log(this.data)
-    })
+  //     this.data = response.filter(this.findEmployer)
+  //     console.log(this.data)
+  //   })
+  // }
+
+  constructor(private route: ActivatedRoute) {
+
   }
+
   ngOnInit(): void {
 
     let id = +this.route.snapshot.paramMap.get('id');
--- src/app/views/admin/view-waiters/view-waiters.component.ts
+++ src/app/views/admin/view-waiters/view-waiters.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { JsonApiServiceService } from '../../../JsonApiService.service'
+
 
 @Component({
   selector: 'app-view-waiters',
@@ -6,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-waiters.component.scss']
 })
 export class ViewWaitersComponent implements OnInit {
-  
-  constructor() { }
+
+    //funcion para obtener no admnistrador
+    findEmployer = (employer: any): any => employer.roles.admin === false
+    // varialbe data almacena array de los meseros
+    data: any;
+
+    constructor(public json: JsonApiServiceService) {
+      this.json.getJson('http://localhost:3000/users#').subscribe((response: any) => {
+
+        this.data = response.filter(this.findEmployer)
+        console.log(this.data)
+      })
+    }
 
   ngOnInit(): void {
   }

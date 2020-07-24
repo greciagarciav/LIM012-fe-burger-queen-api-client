de55e5c6f1e76c6c0face6a00a42a1de473d6dba
Milagros932 <milinr29@gmail.com>
2020-07-24 11:49:40 -0500

    function get and post

--- src/app/JsonApiService.service.ts
+++ src/app/JsonApiService.service.ts
@@ -1,26 +1,54 @@
 import { Injectable } from '@angular/core';
-import {HttpClient}  from '@angular/common/http';
-
+import {
+  HttpClient, HttpHeaders, HttpErrorResponse,} from '@angular/common/http';
+import { User } from './model/user'
+import { catchError, tap, map } from 'rxjs/operators';
+import { throwError, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JsonApiService {
-    url: string = 'http://localhost:3000/users#'
-  constructor(private http: HttpClient){}
 
-  getEmployed(url:string){
-     return this.http.get(url)
+  headers = new HttpHeaders(
+    {
+      'Authorization': 'Bearer 2635645',
+      'Content-Type': 'application/json'
+    })
+
+  constructor(private http: HttpClient) { }
+
+  url: string = 'http://localhost:3000/users#'
+
+  getUser(): Observable<any> {
+    return this.http.get(this.url, { headers: this.headers })
+      .pipe(
+        catchError(this.handleError)
+      )
   }
-  postEmployed(url:string,object:any){
-    return this.http.post(url,object,)
-    //   {
-    //   header:{
-    //     Authorization: Bearer <token>
-    //   }
-    // })
- }
- deleteEmployed(url:string,id:any){
-  return this.http.delete(url,id)
+
+  postUser(user) {
+    return this.http.post(this.url, (user), { headers: this.headers,observe: 'response'})
+    .pipe( 
+      map(resp=>resp),
+      catchError(this.handleError)
+      // tap(resp => resp)
+    )
+  }
+
+  handleError(errorRes: HttpErrorResponse) {
+    if (errorRes.error instanceof ErrorEvent) { //boolean que da el tipo de error
+      return throwError('Error: ' + errorRes.error.message)// error lado del cliente
+    } else {
+      return throwError({// error lado del servidor
+        status: errorRes.status,
+        statusText: errorRes.statusText,
+        message: errorRes.message,
+      })
+    }
+  }
 }
-}
+//  deleteEmployed(url:string,id:any){
+//   return this.http.delete(url,id,{})
+// }
+// }

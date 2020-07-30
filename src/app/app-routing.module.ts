bba3d8c82a32da4ff8ade037737af50ea2a7252c
greciagarciav <garciav.grecia@gmail.com>
2020-07-29 22:27:35 -0500

    delete not used code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from "../app/views/admin/admin.component";
-import { ViewStaffComponent } from "../app/views/admin/view-staff/view-staff.component";
-import { ViewInventaryComponent } from "../app/views/admin/view-inventary/view-inventary.component";
-import { StaffList } from "../app/components/staff-list/staff-list.component";
 import { ModalComponent } from "../app/components/modal/modal.component";
 import { EditUser } from "../app/components/edit-user/edit-user.component";
 
-
 const routes: Routes = [
-
-  // { path: 'logout', component: ViewHomeComponent },
-  // { path: 'inventary/:id', component: StaffList },
-  // { path: 'inventary', component: ViewInventaryComponent },
-  { path: 'cocinero', component: AdminComponent},
-  { path: 'mesero', component: ModalComponent},
-  { path: 'admin/:id', component: AdminComponent },
-  { path: 'admin/:id/:id', component: AdminComponent },
-  // { path: 'meser0/:id', component: ModalComponent },
- { path: '',   redirectTo: 'admin/staff', pathMatch: 'full' },
-
+  { path: 'cocinero', component: AdminComponent },
+  { path: 'mesero', component: AdminComponent },
+  { path: 'admin/staff', component: AdminComponent },
+  { path: 'admin/inventario', component: AdminComponent },
+  { path: '',   redirectTo: 'admin/staff', pathMatch: 'full' }
 ];
 
 @NgModule({

0fda08d75285945b97d39c102eb4d855b90d9392
greciagarciav <garciav.grecia@gmail.com>
2020-08-05 00:21:32 -0500

    create cheff component and view order status component

---
+++ src/app/views/cheff/view-order-status/view-order-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-order-status',
+  templateUrl: './view-order-status.component.html',
+  styleUrls: ['./view-order-status.component.css']
+})
+export class ViewOrderStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

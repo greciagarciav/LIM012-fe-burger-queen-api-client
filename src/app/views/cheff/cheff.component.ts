197e30e00822869e88f513f3e89517163048c0de
greciagarciav <garciav.grecia@gmail.com>
2020-08-05 23:18:47 -0500

    create cheff component and order status component styles and structures

--- src/app/views/cheff/cheff.component.ts
+++ src/app/views/cheff/cheff.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cheff',
   templateUrl: './cheff.component.html',
-  styleUrls: ['./cheff.component.css']
+  styleUrls: ['./cheff.component.scss']
 })
 export class CheffComponent implements OnInit {
 
--- src/app/views/cheff/view-order-status/view-order-status.component.ts
+++ src/app/views/cheff/view-order-status/view-order-status.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-view-order-status',
   templateUrl: './view-order-status.component.html',
-  styleUrls: ['./view-order-status.component.css']
+  styleUrls: ['./view-order-status.component.scss']
 })
 export class ViewOrderStatusComponent implements OnInit {
 

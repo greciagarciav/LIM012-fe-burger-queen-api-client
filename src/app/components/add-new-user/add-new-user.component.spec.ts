147f0a0db72b487d42df5c2d19961da245deec58
greciagarciav <garciav.grecia@gmail.com>
2020-08-30 17:57:54 -0500

    test for add new user

--- src/app/components/add-new-user/add-new-user.component.spec.ts
+++ src/app/components/add-new-user/add-new-user.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed, tick } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AddNewUserComponent } from './add-new-user.component';
+import { User } from '../../model/user'
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 
@@ -29,15 +30,38 @@ describe('AddNewUserComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
 
-component.ngOnInit()
+    component.ngOnInit()
 
   });
 
-it('should form invalid when empty',()=>{
-expect(component.addForm.valid).toBeFalsy()
-})
+  it('should form invalid when empty',()=>{
+  expect(component.addForm.valid).toBeFalsy()
+  });
 
+  it('email field validity',()=>{
+    let email = component.addForm.controls['email'];
+    expect(email.valid).toBeFalsy();
 
+    let errors = {};
+    errors = email.errors || {};
+    expect(errors['required']).toBeTruthy();
+
+  });
+
+  it('subbmitting a form emits a user', () => {
+    expect(component.addForm.valid).toBeFalsy();
+    component.addForm.controls['email'].setValue('test@test.com');
+    component.addForm.controls['password'].setValue('123456789');
+    expect(component.addForm.valid).toBeTruthy();
+
+    let userAdded : User;
+    userAdded = component.addForm.value;
+
+    component.addUser();
+
+    expect(userAdded.email).toBe('test@test.com');
+    expect(userAdded.password).toBe('123456789');
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();

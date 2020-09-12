1345ea45cf3b73688d3dd4d41a86bd842e04613a
greciagarciav <garciav.grecia@gmail.com>
2020-09-11 23:03:30 -0500

    change to true production mode

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiUrl:'http://ec2-13-58-43-131.us-east-2.compute.amazonaws.com/',
   // apiUrl:'http://localhost:3000/',
 };
